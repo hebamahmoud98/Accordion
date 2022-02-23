c6e75f0e636625217a161ae04dfce62541e2d061
hebamahmoud98 <89314697+hebamahmoud98@users.noreply.github.com>
2022-02-23 09:36:46 +0200

    first Version

---
+++ main.js
@@ -0,0 +1,13 @@
+let heading=document.getElementsByClassName('Accordion');
+var i;
+for(i=0 ; i<heading.length ;i++){
+   heading[i].addEventListener('click',function(){
+       this.classList.toggle('active');
+       var dec=this.nextElementSibling;
+       if(dec.style.maxHeight){
+        dec.style.maxHeight=null
+       }else{
+        dec.style.maxHeight=dec.scrollHeight +'px  '
+       }
+   }) 
+}
